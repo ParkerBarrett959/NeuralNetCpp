dbaf2a57e0f3ea44b4492b58406b03ba8c16a260
Parker Barrett <parkerb7@vt.edu>
2025-07-06 17:01:22 -0400

    Add example main

---
+++ src/Main.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "Value.h"
+
+// Main function
+int main() {
+  // Load in Training dataset
+  // TODO...
+
+  // Split dataset into training and evaluation portions
+
+  // Initialize a neural network for training
+  // TODO...
+
+  // Main Training Loop
+  static constexpr int NUM_TRAINING_STEPS = 1000;
+  for (int i = 0; i < NUM_TRAINING_STEPS; ++i) {
+    // Forward prediction of loss
+    double loss = 100.0;
+
+    // Zero out gradients in network
+
+    // Perform backpropagation
+
+    // Apply stochastic gradient descent update
+
+    // Print status
+    std::cout << "Training Step: " << i << ", loss = " << loss << std::endl;
+  }
+
+  // Run evaluation dataset and display results
+  // TODO...
+  return 0;
+}
